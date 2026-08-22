d62b9091d5540aeea7fa0d9e68a4a94d2f6c7070
Charxies <carloscelestino@proton.me>
2026-08-22 15:08:18 -0600

    Feat: creating and importing album service and mapper for POST mapping

--- 01-spring-data-JPA-demo/src/main/java/org/example/_1springdatajpa/rest/AlbumRestController.java
+++ 01-spring-data-JPA-demo/src/main/java/org/example/_1springdatajpa/rest/AlbumRestController.java
@@ -1,5 +1,12 @@
 package org.example._1springdatajpa.rest;
 
+import org.example._1springdatajpa.service.AlbumService;
+import tools.jackson.databind.json.JsonMapper;
+
 public class AlbumRestController {
+    private AlbumService albumService;
+    private JsonMapper jsonMapper;
+
+
 }
 
