1c6de7d226a5791de66add198a883674473b7c21
Charxies <carloscelestino@proton.me>
2026-08-22 15:51:52 -0600

    Feat: POST PUT PATCH mappings implemented to the controller

--- 01-spring-data-JPA-demo/src/main/java/org/example/_1springdatajpa/rest/AlbumRestController.java
+++ 01-spring-data-JPA-demo/src/main/java/org/example/_1springdatajpa/rest/AlbumRestController.java
@@ -3,11 +3,11 @@ package org.example._1springdatajpa.rest;
 import org.example._1springdatajpa.entity.Album;
 import org.example._1springdatajpa.service.AlbumService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
 import tools.jackson.databind.json.JsonMapper;
 
 import java.util.List;
+import java.util.Map;
 
 public class AlbumRestController {
     private AlbumService albumService;
@@ -26,6 +26,35 @@ public class AlbumRestController {
         Album album = albumService.AlbumFindID(albumId);
         if (album == null){throw new RuntimeException("No se encontro album ID: "+albumId);}
         return  album;
+    }
+    @PostMapping("/album")
+    public Album postAlbum(@RequestBody Album album){
+        album.setId(0);
+        Album dbAlbum = albumService.albumSave(album);
+        return  dbAlbum;
+    }
+
+    @PutMapping("/album")
+    public Album updateAlbum(@RequestBody Album album){
+
+        Album dbAlbum = albumService.albumSave(album);
+        return dbAlbum;
+    }
+
+    @PatchMapping("/album/{albumId}")
+    public  Album patchAlbum(@PathVariable int albumId, @RequestBody Map<String, Object> patchPayload){
+        Album tempAlbum = albumService.AlbumFindID(albumId);
+        if (tempAlbum == null) {
+            throw new RuntimeException("No se encontro album ID: " + albumId);
+        }
+        if (patchPayload.containsKey("id")) {
+            throw new RuntimeException(
+                    "AlbumID no puede ser modificado. remueve 'id' del body request.");
+        }
+        Album patchedAlbum = jsonMapper.updateValue(tempAlbum, patchPayload);
+        Album dbAlbum = albumService.albumSave(patchedAlbum);
+
+        return dbAlbum;
     }
 }
 
