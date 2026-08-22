2fc9c74afe0081a5b9333f9e21ffa8f42f5cb7b5
Charxies <carloscelestino@proton.me>
2026-08-22 15:27:16 -0600

    Feat: creating and importing mapping tools for GET-TING album DB data

--- 01-spring-data-JPA-demo/src/main/java/org/example/_1springdatajpa/rest/AlbumRestController.java
+++ 01-spring-data-JPA-demo/src/main/java/org/example/_1springdatajpa/rest/AlbumRestController.java
@@ -1,7 +1,13 @@
 package org.example._1springdatajpa.rest;
 
+import org.example._1springdatajpa.entity.Album;
 import org.example._1springdatajpa.service.AlbumService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import tools.jackson.databind.json.JsonMapper;
+
+import java.util.List;
 
 public class AlbumRestController {
     private AlbumService albumService;
@@ -13,6 +19,13 @@ public class AlbumRestController {
         this.jsonMapper = jsonMapper;
     }
 
-
+    @GetMapping("/album")
+    public List<Album> findAll(){return albumService.findAll();}
+    @GetMapping("/album/{albumId}")
+    public Album getAlbum(@PathVariable int albumId){
+        Album album = albumService.AlbumFindID(albumId);
+        if (album == null){throw new RuntimeException("No se encontro album ID: "+albumId);}
+        return  album;
+    }
 }
 
