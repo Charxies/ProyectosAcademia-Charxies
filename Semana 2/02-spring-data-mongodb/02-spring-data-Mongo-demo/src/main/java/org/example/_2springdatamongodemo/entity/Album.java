2015ff1c3782b2d86e59e5c993b074bedb6a8d8b
Charxies <carloscelestino@proton.me>
2026-08-23 23:37:37 -0600

    Edit: refactoring previous entity into new MongoDB context, id changed for String

--- Semana 2/02-spring-data-mongodb/02-spring-data-Mongo-demo/src/main/java/org/example/_2springdatamongodemo/entity/Album.java
+++ Semana 2/02-spring-data-mongodb/02-spring-data-Mongo-demo/src/main/java/org/example/_2springdatamongodemo/entity/Album.java
@@ -8,7 +8,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 public class Album {
 
     @Id
-    private int id;
+    private String id;
 
     private String titulo;
 
@@ -30,11 +30,11 @@ public class Album {
         this.anio= anio;
     }
 
-    public int getId() {
+    public String getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(String id) {
         this.id = id;
     }
 
