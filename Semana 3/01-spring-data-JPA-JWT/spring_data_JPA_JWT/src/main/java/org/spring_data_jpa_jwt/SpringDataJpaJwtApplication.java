1a3de036cea31e87793b9eef237b8191d117c24a
Charxies <carloscelestino@proton.me>
2026-08-28 12:46:30 -0600

    Fix: reverted local changes, git is insane sometimes

---
+++ Semana 3/01-spring-data-JPA-JWT/spring_data_JPA_JWT/src/main/java/org/spring_data_jpa_jwt/SpringDataJpaJwtApplication.java
@@ -0,0 +1,13 @@
+package org.spring_data_jpa_jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaJwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataJpaJwtApplication.class, args);
+    }
+
+}
