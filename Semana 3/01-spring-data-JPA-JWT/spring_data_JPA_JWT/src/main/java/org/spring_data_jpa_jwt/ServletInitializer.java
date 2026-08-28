c9eb6564d2acc5a9f55484b87abecfba23bf8479
Charxies <carloscelestino@proton.me>
2026-08-28 09:33:12 -0600

    Edit: deleting unused files and creating new maven module

---
+++ Semana 3/01-spring-data-JPA-JWT/spring_data_JPA_JWT/src/main/java/org/spring_data_jpa_jwt/ServletInitializer.java
@@ -0,0 +1,13 @@
+package org.spring_data_jpa_jwt;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SpringDataJpaJwtApplication.class);
+    }
+
+}
---
+++ Semana 3/01-spring-data-JPA-JWT/spring_data_JPA_JWT/src/test/java/org/spring_data_jpa_jwt/SpringDataJpaJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package org.spring_data_jpa_jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpaJwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
